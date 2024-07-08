7ff80371c5495948d47624b860882e3437714a08
Neogr1 <mslee720@gmail.com>
2024-07-08 18:26:20 +0900

    add week 2 mission 1~4 files

---
+++ missions/w2/3_multiprocessing_queue/m3_mp_queue.py
@@ -0,0 +1,24 @@
+'''
+multiprocessing을 사용하기 위한 프로그래밍 지침
+https://docs.python.org/ko/3/library/multiprocessing.html#multiprocessing-programming
+'''
+
+from multiprocessing import Queue
+
+if __name__ == '__main__':
+    items = ['red', 'green', 'blue', 'black']
+    q = Queue()
+
+    n = 1
+    print("pushing items to queue")
+    for item in items:
+        q.put(item)
+        print(f"item no: {n} {item}")
+        n += 1
+    
+    n = 0
+    print("popping items from queue")
+    while not q.empty():
+        item = q.get()
+        print(f"item no: {n} {item}")
+        n += 1
